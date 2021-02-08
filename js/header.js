72223aa72659f502e836c02514515305e06d2331
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-08 16:07:43 +0200

    Format js

--- js/header.js
+++ js/header.js
@@ -1,30 +1,30 @@
-window.addEventListener('scroll', handleHeaderScroll)
+window.addEventListener('scroll', handleHeaderScroll);
 
-let lastScrollTop = 0
-const delta = 5
+let lastScrollTop = 0;
+const delta = 5;
 
 function handleHeaderScroll() {
-    const header = document.getElementById('header')
-    const topOffset = window.pageYOffset
+    const header = document.getElementById('header');
+    const topOffset = window.pageYOffset;
 
     if (Math.abs(lastScrollTop - topOffset) <= delta) {
-        return
+        return;
     }
 
     if (topOffset > lastScrollTop && topOffset > header.clientHeight) {
         // Scroll down
-        header.style.top = `${-header.clientHeight}px`
+        header.style.top = `${-header.clientHeight}px`;
     } else {
         if (topOffset < lastScrollTop) {
             // Scroll up
-            header.style.position = 'fixed'
-            header.style.top = 0
+            header.style.position = 'fixed';
+            header.style.top = 0;
         }
     }
 
     if (topOffset <= delta) {
-        header.style.position = 'absolute'
+        header.style.position = 'absolute';
     }
 
-    lastScrollTop = topOffset
+    lastScrollTop = topOffset;
 }
--- js/video-layout.js
+++ js/video-layout.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", adjustVideoPlayerSize)
-window.addEventListener('resize', adjustVideoPlayerSize)
+document.addEventListener("DOMContentLoaded", adjustVideoPlayerSize);
+window.addEventListener('resize', adjustVideoPlayerSize);
 
 
 function adjustVideoPlayerSize() {
-    const videoWrapper = document.querySelector('#video')
-    const video = document.querySelector('#video_html5_api')
-    const width = document.body.clientWidth
+    const videoWrapper = document.querySelector('#video');
+    const video = document.querySelector('#video_html5_api');
+    const width = document.body.clientWidth;
 
     if (video) {
-        video.style.width = `${width}px`
-        video.style.height = `${width / 16 * 9}px`
+        video.style.width = `${width}px`;
+        video.style.height = `${width / 16 * 9}px`;
     }
 
     if (videoWrapper) {
-        videoWrapper.style.width = `${width}px`
-        videoWrapper.style.height = `${width / 16 * 9}px`
+        videoWrapper.style.width = `${width}px`;
+        videoWrapper.style.height = `${width / 16 * 9}px`;
     }
 }
