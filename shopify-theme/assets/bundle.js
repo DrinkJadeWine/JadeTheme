30755dddc8dc732bc367740684bb5f651c540eb9
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-08 15:17:04 +0200

    Bundle all js

--- shopify-theme/assets/bundle.js
+++ shopify-theme/assets/bundle.js
@@ -29,3 +29,23 @@ function handleHeaderScroll() {
 
   lastScrollTop = topOffset;
 }
+"use strict";
+
+document.addEventListener("DOMContentLoaded", adjustVideoPlayerSize);
+window.addEventListener('resize', adjustVideoPlayerSize);
+
+function adjustVideoPlayerSize() {
+  var videoWrapper = document.querySelector('#video');
+  var video = document.querySelector('#video_html5_api');
+  var width = document.body.clientWidth;
+
+  if (video) {
+    video.style.width = "".concat(width, "px");
+    video.style.height = "".concat(width / 16 * 9, "px");
+  }
+
+  if (videoWrapper) {
+    videoWrapper.style.width = "".concat(width, "px");
+    videoWrapper.style.height = "".concat(width / 16 * 9, "px");
+  }
+}
--- tasks/js.js
+++ tasks/js.js
@@ -1,12 +1,14 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import path from 'path'
+import concat from 'gulp-concat'
 
 export function js() {
     return gulp.src(path.resolve(__dirname, '../js/**/*.js'))
         .pipe(babel({
             presets: ['@babel/preset-env']
         }))
+        .pipe(concat('bundle.js'))
         .pipe(gulp.dest(path.resolve(__dirname, '../shopify-theme/assets')))
 }
 
