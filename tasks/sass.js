8355989e01084c667050356bb2acdd95d03f189c
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-08 14:23:05 +0200

    Use autoprefixer

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,11 +1,13 @@
 import gulp from 'gulp'
 import path from 'path'
 import sass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
 
 
 export function compileSass() {
     return gulp.src(path.resolve(__dirname, '../sass/**/*.scss'))
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(gulp.dest(path.resolve(__dirname, '../shopify-theme/assets')))
 }
 
