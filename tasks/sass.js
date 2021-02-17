8faf76944ce62087bf5fd7350a5d6540d9a2c30f
Nazar Kaluzghnyy <kaluzghnyy@gmail.com>
2021-02-17 14:50:24 +0200

    PLP setup

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp'
 import path from 'path'
 import sass from 'gulp-sass'
+import concat from 'gulp-concat'
 import autoprefixer from 'gulp-autoprefixer'
 
 
@@ -8,7 +9,8 @@ export function compileSass() {
     return gulp.src(path.resolve(__dirname, '../sass/**/*.scss'))
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(gulp.dest(path.resolve(__dirname, '../shopify-theme/assets')))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(path.resolve(__dirname, '../shopify-theme/assets/')))
 }
 
 export function watchSass() {
